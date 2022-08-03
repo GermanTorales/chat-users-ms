67e076450060f41c803722a1edaf49ff5ee804d7
German Torales <germantorales73@gmail.com>
2022-08-03 20:18:09 -0300

    NO-TASK Create tests

--- __tests__/factories/user.factory.ts
+++ __tests__/factories/user.factory.ts
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker';
-import { CreateUserDTO } from 'src/application/dtos';
+import { CreateUserDTO } from '../../src/application/dtos';
+import { User } from '../../src/domain/entities';
 
 export const createFakeUser = (): CreateUserDTO => ({
   name: faker.name.firstName(),
@@ -7,3 +8,11 @@ export const createFakeUser = (): CreateUserDTO => ({
   password: faker.internet.password(),
   confirmPassword: faker.internet.password(),
 });
+
+export const createFakeUsersArray = (length = 10): User[] => {
+  return [...new Array(length)].map((): User => {
+    const { username, name } = createFakeUser();
+
+    return { username, name };
+  });
+};
---
+++ __tests__/unit/application/use-cases/getAllUsers.spec.ts
@@ -0,0 +1,37 @@
+import { Test } from '@nestjs/testing';
+import { Port } from '../../../../src/domain/enums/ports';
+import { IUserRepository } from '../../../../src/domain/interfaces';
+import { createFakeUsersArray } from '../../../factories';
+import { GetAllUsers } from '../../../../src/application/use-cases';
+import { User } from '../../../../src/domain/entities';
+
+describe('CreateUser use-case Test', () => {
+  let userRepository: IUserRepository;
+  let getAllUsersUseCase: GetAllUsers;
+  let usersFakeArray: User[];
+
+  beforeEach(async () => {
+    usersFakeArray = createFakeUsersArray();
+
+    const module = await Test.createTestingModule({
+      providers: [
+        GetAllUsers,
+        {
+          provide: Port.User,
+          useFactory: () => ({ find: jest.fn() }),
+        },
+      ],
+    }).compile();
+
+    userRepository = module.get<IUserRepository>(Port.User);
+    getAllUsersUseCase = module.get<GetAllUsers>(GetAllUsers);
+  });
+
+  it('should get all users', async () => {
+    jest.spyOn(userRepository, 'find').mockImplementation(async () => usersFakeArray);
+
+    const users = await getAllUsersUseCase.exec();
+
+    expect(users).toHaveLength(usersFakeArray.length);
+  });
+});
